a3d2da9d6f73b1b0d063715e6f7e7bc133e174b5
Jeffrey Degoma <jeff@Jeffreys-MBP.home>
2017-04-16 17:48:49 -0400

    added nav component

---
+++ client/src/components/navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Navbar, NavItem} from 'react-materialize'
+
+export default function Navigation() {
+
+	return(
+
+	<Navbar brand='logo' right>
+  		<NavItem href='get-started.html'>Getting started</NavItem>
+  		<NavItem href='components.html'>Components</NavItem>
+	</Navbar>
+    
+     )
+}

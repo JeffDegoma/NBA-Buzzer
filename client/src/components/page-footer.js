b7c5523ae56651c00f5734b48d55a50927cfa4ea
Jeffrey Degoma <jeff@Jeffreys-MacBook-Pro.local>
2017-05-24 13:42:06 -0400

    added footer

--- client/src/components/page-footer.js
+++ client/src/components/page-footer.js
@@ -7,7 +7,7 @@ export default function PageFooter() {
     return (
         <Footer copyrights="&copy; 2017 Copyright Text">
             <h6 className="white-text author-name">Made By Jeffrey Degoma</h6>
-            <a className="grey-text text-lighten-3" href="#">GitHub</a>
+            <a target="_blank" className="grey-text text-lighten-3" href="https://github.com/JeffDegoma/nba-twitter">GitHub</a>
         </Footer>
     );
 }
